3bf5ff1c52441e0a3c839f3eecc8c3d147bd4f90
rhl120 <rhl120@protonmail.com>
2022-07-10 17:08:32 +0200

    Use NULLD_*_PATH in load.sh

--- load.sh
+++ load.sh
@@ -1,7 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 insmod $* || exit 1
 major=$(awk "\$2==\"nulld\" {print \$1}" /proc/devices)
-mknod /dev/nulld_null c $major 0
-mknod /dev/nulld_zero c $major 1
-chmod 222 /dev/nulld_null
-chmod 444 /dev/nulld_zero
+if [ -z $NULLD_REPEAT_PATH ]
+then
+	NULLD_REPEAT_PATH="/dev/nulld_zero"
+fi
+
+if [ -z $NULLD_HOLE_PATH ]
+then
+	NULLD_HOLE_PATH="/dev/nulld_null"
+fi
+mknod $NULLD_HOLE_PATH c $major 0
+mknod $NULLD_REPEAT_PATH c $major 1
+chmod 222 $NULLD_HOLE_PATH
+chmod 444 $NULLD_REPEAT_PATH
