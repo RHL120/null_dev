cc8bb4a2c2936c6e4cd5ded4b4c76cb6b649ce05
rhl120 <rhl120@protonmail.com>
2022-07-10 17:08:47 +0200

    Use NULLD_.*_PATH in unload.sh

--- unload.sh
+++ unload.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
+if [ -z $NULLD_REPEAT_PATH ]
+then
+	NULLD_REPEAT_PATH="/dev/nulld_zero"
+fi
+
+if [ -z $NULLD_HOLE_PATH ]
+then
+	NULLD_HOLE_PATH="/dev/nulld_null"
+fi
 rmmod nulld
-rm /dev/nulld_null
-rm /dev/nulld_zero
+rm $NULLD_HOLE_PATH
+rm $NULLD_REPEAT_PATH
